e5834e8263be2ca451386a9bfc204129fc955a64
Brendan Brown <brendan94brown@gmail.com>
2025-04-05 16:55:00 -0700

    Refactored independent functions to be methods inside the library object, refactoring tests to fit

--- library.js
+++ library.js
@@ -20,7 +20,60 @@ const library = {
                       name: "Other Playlist",
                       tracks: ["t03"]
                     }
-             }
+             },
+  printPlaylists: function() {
+    let playlistIDs = Object.keys(this.playlists);
+    for (let i = 0; i < playlistIDs.length; i++) {
+      let playlistID = playlistIDs[i]
+      let playlist = this.playlists[playlistID];
+      console.log(`${playlistID}: ${playlist.name} - ${playlist.tracks.length}`);
+    }
+  },
+  printTracks: function() {
+    for (let trackID in this.tracks) {
+      let track = this.tracks[trackID];
+      console.log(`${trackID}: ${track.name} by ${track.artist} (${track.album})`);
+    }
+  },
+  printPlaylist: function(playlistId) {
+    let playlist = this.playlists[playlistId];
+    let playlistName = playlist.name;
+    let howManyTracks = playlist.tracks.length;
+    console.log(`${playlistId}: ${playlistName} - ${howManyTracks} tracks`);
+  
+    for (let trackId of playlist.tracks) {
+      let track = this.tracks[trackId];
+      console.log(`${trackId}: ${track.name} by ${track.artist} (${track.album})`);
+    }
+  },
+  addTrackToPlaylist: function(trackId, playlistId) {
+    let playlist = this.playlists[playlistId];
+  
+    if (!playlist.tracks.includes(trackId)) {
+      playlist.tracks.push(trackId);
+      console.log(`Added ${trackId} to playlist ${playlistId}`);
+    }
+  },
+  generateUid: function() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  },
+  addTrack: function(name, artist, album) {
+    const newTrack = this.generateUid();
+    this.tracks[newTrack] = {
+      id: newTrack,
+      name: name,
+      artist: artist,
+      album: album
+    };
+  },
+  addPlaylist: function(name) {
+    const newPlaylist = generateUid();
+    this.playlists[newPlaylist] = {
+      id: newPlaylist,
+      name: name,
+      tracks: []
+    };
+  }
 };
 
 /////////////////////////////
@@ -39,7 +92,7 @@ const printPlaylists = function() {
     console.log(`${playlistID}: ${playlist.name} - ${playlist.tracks.length}`);
   }
 };
-//printPlaylists();
+//library.printPlaylists();
 
 // prints a list of all tracks, using the following format:
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
@@ -52,7 +105,7 @@ const printTracks = function() {
     console.log(`${trackID}: ${track.name} by ${track.artist} (${track.album})`);
   }
 };
-//printTracks();
+//library.printTracks();
 
 
 // prints a list of tracks for a given playlist, using the following format:
@@ -70,8 +123,8 @@ const printPlaylist = function(playlistId) {
     console.log(`${trackId}: ${track.name} by ${track.artist} (${track.album})`);
   }
 };
-//printPlaylist("p01");
-//printPlaylist("p02");
+//library.printPlaylist("p01");
+//library.printPlaylist("p02");
 
 // adds an existing track to an existing playlist
 const addTrackToPlaylist = function(trackId, playlistId) {
@@ -82,7 +135,7 @@ const addTrackToPlaylist = function(trackId, playlistId) {
     console.log(`Added ${trackId} to playlist ${playlistId}`);
   }
 };
-//addTrackToPlaylist("t03", "p01");
+//library.addTrackToPlaylist("t03", "p01");
 
 
 // generates a unique id
@@ -102,7 +155,7 @@ const addTrack = function(name, artist, album) {
     album: album
   };
 };
-//addTrack("What am I doing", "Brendan", "Maybe this isn't for me");
+//library.addTrack("What am I doing", "Brendan", "Maybe this isn't for me");
 //console.log(library.tracks);
 
 
@@ -115,8 +168,8 @@ const addPlaylist = function(name) {
     tracks: []
   };
 };
-//addPlaylist("Brendan's Confusion: Greatest Hits");
-//console.log(library.playlists);
+library.addPlaylist("Brendan's Confusion: Greatest Hits");
+console.log(library.playlists);
 
 // STRETCH:
 // given a query string string, prints a list of tracks
